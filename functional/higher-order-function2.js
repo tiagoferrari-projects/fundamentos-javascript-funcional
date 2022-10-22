bcbd9835cec4a14c07a0a9e325886848d694a11e
Tiago Ferrari <tiago-projects@outlook.com>
2022-10-22 15:28:56 -0300

    função de alta ordem pt 2

---
+++ functional/higher-order-function2.js
@@ -0,0 +1,18 @@
+/*
+ Return a function from another function
+ 
+ Curring
+
+ */
+
+function finalPrice(tax,) {
+    return function (price) {
+        return price * (1 + tax)
+    }
+}
+
+const nycFinalPrice = finalPrice(0.0875)
+
+console.log(nycFinalPrice(25.1))
+console.log(nycFinalPrice(21.7))
+console.log(nycFinalPrice(107.9))
